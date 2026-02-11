4c0dc29afcb8c9b2112d034bd3d13979e7b94d89
Hayden <hchalin98@gmail.com>
2026-02-10 17:16:39 -0800

    Initial Commit

---
+++ Aluminium/AluminiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluminiumApp.swift
+//  Aluminium
+//
+//  Created by Hayden Chalin on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct AluminiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aluminium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aluminium
+//
+//  Created by Hayden Chalin on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
